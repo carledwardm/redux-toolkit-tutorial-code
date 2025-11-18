5f7296d5bf4dccb9b7acb2734b50a9cc5cb285ad
carledwardm <cmillardv77@gmail.com>
2025-11-18 06:25:24 -0500

    Add modal to app - pending functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import CartContainer from './components/CartContainer';
+import Modal from './components/Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals } from './features/cart/cartSlice';
 import { useEffect } from 'react';
@@ -12,6 +13,7 @@ function App() {
   }, [cartItems]);
   return ( 
   <main>
+      <Modal />
       <Navbar />
       <CartContainer />
   </main>
---
+++ src/components/Modal.js
@@ -0,0 +1,15 @@
+const Modal = () => {
+    return (
+        <aside className="modal-container">
+            <div className="modal">
+                <h4>Remove all items from your shopping cart</h4>
+                <div className="btn-container">
+                    <button type="button" className="btn confirm-btn">confirm</button>
+                    <button type="button" className="btn clear-btn">clear</button>
+                </div>
+            </div>
+        </aside>
+    );
+}
+
+export default Modal;
