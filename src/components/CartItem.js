27acfe52a21df09d34e608db8653cf5f95e5fb29
carledwardm <cmillardv77@gmail.com>
2025-11-18 06:02:24 -0500

    Add and Remove individual item functionality added

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,6 @@
 import { ChevronDown, ChevronUp } from "../icons";
-import { removeItem } from '../features/cart/cartSlice';
-import { useDispatch }  from 'react-redux';
+import { removeItem, increase, decrease  } from '../features/cart/cartSlice';
+import { useDispatch}  from 'react-redux';
 
 const CartItem = ({id, img, title, price, amount}) => {
     const dispatch = useDispatch()
@@ -15,11 +15,20 @@ const CartItem = ({id, img, title, price, amount}) => {
                 }}>remove</button>
             </div>
             <div>
-                <button className='amount-btn'>
+                <button className='amount-btn' onClick={()=>{
+                    dispatch(increase({ id }))
+                }}>
                     <ChevronUp />
                 </button>
                 <p className='amount'>{amount}</p>
-                <button className='amount-btn'>
+                <button className='amount-btn' onClick={()=>{
+                    if (amount === 1) {
+                        dispatch(removeItem(id));
+                        return;
+                    }
+                    dispatch(decrease({id}))
+                    
+                }}>
                     <ChevronDown />
                 </button>
             </div>
